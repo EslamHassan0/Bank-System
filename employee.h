bd778c2e16ee4d05ac737873db5411ac16d48035
MohamedElyamani <58939494+MohamedElyamani@users.noreply.github.com>
2025-02-25 21:44:04 +0200

    Update employee.h

--- employee.h
+++ employee.h
@@ -1,10 +1,15 @@
+#pragma once
 #include <iostream>
 #include "Person.h"
+#include "fstream"
 using namespace std;
+
 
 class Employee : public Person{
 protected:
     double salary;
+    string fileName;
+    char choice;
 public:
     Employee() {}
     Employee(int id, string name, string password, double salary) : Person(id, name, password) {
@@ -12,7 +17,6 @@ public:
     }
     // setters
     void setSalary(double salary) { 
-
         while (true) {
             try {
                 cout << "Enter Salary: ";
@@ -26,6 +30,22 @@ public:
                 cout << "Error: " << e.what() << endl;
             }
         }
+    }
+    void addEmployee(){
+        fileName = "Emps.txt";
+        fstream file(fileName, ios::app);
+        file << getId() << "|" << getName() << "|" << getPassword() << "|" << getSalary() << endl;
+        cout << "Employee added successfully!" << endl;
+        file.close();
+    }
+    void invoke() {
+        do {
+            collectData();
+            setSalary(salary);
+            addEmployee();
+            cout << "Do you want to add another employee? (y/n): ";
+            cin >> choice;
+        } while (choice == 'y' || choice == 'Y');
     }
     // getters
     double getSalary() {
