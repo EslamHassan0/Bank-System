ccf6b843edbc832cf4dfd2f6c7a2eb3684171455
MohamedElyamani <58939494+MohamedElyamani@users.noreply.github.com>
2025-02-25 21:44:19 +0200

    Update index.cpp

--- index.cpp
+++ index.cpp
@@ -1,15 +1,11 @@
 #include <iostream>
 #include "Person.h"
 #include "Employee.h"
+ 
 using namespace std;
 
 int main() {
-        Employee p1;
-    p1.setId(0);  // Set ID using the setter
-    p1.setName("");  // Set Name using the setter
-    p1.setPassword("");  // Set Password using the setter
-    p1.setSalary(0);
-    p1.display();
-   
-    
+    Employee p1;
+    p1.invoke();
+    return 0;
 }
